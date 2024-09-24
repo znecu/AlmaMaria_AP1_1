7055ed6b642316886005ec0277ee84dbb76237d9
Alma Judith Maria Marte <alma_maria@ucne.edu.do>
2024-09-24 19:05:49 -0400

    Inyectando el modelo

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AlmaMaria_AP1_1.Models;
+namespace AlmaMaria_AP1_1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+
+    public DbSet<Registro> Registro { get; set; }
+}
