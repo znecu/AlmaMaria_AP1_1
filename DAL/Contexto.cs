b6fa63338aea130b860b6bbcdb8e3d53c8ebfa2f
Alma Judith Maria Marte <alma_maria@ucne.edu.do>
2024-09-30 18:34:45 -0400

    Se reenombró el nombre en el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,6 +6,6 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) {}
 
-    public DbSet<Registro> Registro { get; set; }
+    public DbSet<Prestamos> Prestamos { get; set; }
 
 }
