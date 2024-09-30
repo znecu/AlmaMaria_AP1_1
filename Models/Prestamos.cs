6d88f4addab5adb236dfc2477500ec7c1c632e6a
Alma Judith Maria Marte <alma_maria@ucne.edu.do>
2024-09-30 18:30:49 -0400

    Creé el modelo de prestamos

---
+++ Models/Prestamos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlmaMaria_AP1_1.Models;
+
+public class Prestamos
+{
+    [Key]
+    public int PrestamoId { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[a-Az-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    public string? Deudor { get; set; }  
+
+    [Required]
+    [RegularExpression(@"^[a-Az-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    public string? Concepto { get; set; }
+
+
+    [Required]
+    [RegularExpression(@"^[0-9]", ErrorMessage =("Este campo solo permite números. "))]
+    public decimal Monto { get; set; }
+}
