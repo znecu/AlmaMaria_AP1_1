b2267f98e2137fa0220854f195b22146faff64af
Alma Judith Maria Marte <alma_maria@ucne.edu.do>
2024-09-30 20:30:17 -0400

    Arreglé las expresiones regulares

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -8,15 +8,15 @@ public class Prestamos
     public int PrestamoId { get; set; }
 
     [Required]
-    [RegularExpression(@"^[a-Az-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
     public string? Deudor { get; set; }  
 
     [Required]
-    [RegularExpression(@"^[a-Az-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
     public string? Concepto { get; set; }
 
 
     [Required]
-    [RegularExpression(@"^[0-9]", ErrorMessage =("Este campo solo permite números. "))]
+    [RegularExpression(@"^\d+$", ErrorMessage =("Este campo solo permite números. "))]
     public decimal Monto { get; set; }
 }
