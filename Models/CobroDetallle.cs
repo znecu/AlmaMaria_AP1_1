9b04a2999edb02e08eae8aee7e0e921c8518cab0
Alma Judith Maria Marte <alma_maria@ucne.edu.do>
2024-10-09 16:10:16 -0400

    Creé el modelo de CobroDetalle

---
+++ Models/CobroDetallle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlmaMaria_AP1_1.Models;
+
+public class CobroDetallle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int CobroId {  get; set; }
+
+    public int PrestamoId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [Range(minimum: 0.1, maximum: 9999999999, ErrorMessage = "Ingrese un cantidad válida.")]
+    public Decimal ValorCobrado { get; set; }
+
+    [ForeignKey("CobroId")]
+    public ICollection<Cobros> Cobros { get; set; } = new List<Cobros>();
+
+    [ForeignKey("PrestamoId")]
+    public ICollection<Prestamos> Prestamos { get; set; } = new List<Prestamos>();
+}
