7f195dbea7e9a2a18ff28d5c168dd1ab800ba6da
Alma Judith Maria Marte <alma_maria@ucne.edu.do>
2024-10-09 16:10:45 -0400

    Creé el modelo de deudores

---
+++ Models/Deudores.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlmaMaria_AP1_1.Models;
+
+public class Deudores
+{
+    [Key]
+    public int DeudorId { get; set; }
+
+    public string Nombres { get; set; }
+}
