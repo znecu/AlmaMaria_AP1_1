8cb3f96069aeb29974a26f8af7e7eaccbaf428f2
znecu <alma_maria@ucne.edu.do>
2024-10-11 00:09:09 -0400

    Agregué llaves foráneas

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AlmaMaria_AP1_1.Models;
 
@@ -11,7 +12,6 @@ public class Prestamos
     [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
     public string? Concepto { get; set; }
 
-
     [Required(ErrorMessage = "Este campo es obligatorio. ")]
     [Range(minimum: 0.1, maximum: 9999999999, ErrorMessage = "Ingrese un numero valido")]
     public decimal Monto { get; set; }
@@ -19,5 +19,14 @@ public class Prestamos
     [Required(ErrorMessage = "Este campo es obligatorio. ")]
     [Range(minimum: 0.1, maximum: 9999999999, ErrorMessage = "Ingrese un número válido. ")]
     public decimal Balance { get; set; }
+
+    public int CobroId { get; set; }
+    [ForeignKey("CobroId")]
+    public Cobros? Cobros { get; set; }
+
+    public int DeudorId { get; set; }
+    [ForeignKey("DeudorId")]
+    public Deudores? Deudores { get; set; }
+
 
 }
